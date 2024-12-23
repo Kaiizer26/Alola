7d53f5112348363c0b04727904c1818ceaec85ee
Dylan <zakadylan2005@gmail.com>
2024-12-23 01:29:07 +0100

    ajout cards

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -1,119 +1,160 @@
 <main class="main-accueil">
-            <!-- Hero banner -->
-            <section class="first-page">
-                <section class="hero-banner">
-                    <section class="txt-1">
-                        <article class="slide-txt">
-                            <div class="">Équipez</div>
-                            <div class="">Sautez</div>
-                            <div class="">Frappez</div>
-                            <div class="">Gagnez</div>
-                        </article> 
-                            
-                        <h1>Prêt </br> à jouer ?</h1>
-                        <button class="btn1">Découvrir les produits <i class="fa-solid fa-arrow-right-long"></i></button>
-                    </section>
-                    <section class="txt-2">
-                    Commencez dès maintenant à parcourir notre catalogue et trouvez l’équipement parfait pour votre prochain smash !
-                    </section>                
-                </section>
-                <!-- Fonctionnalités -->
-                <section class="features">
-                    <article>
-                        <section>
-                            <img src="assets/images/shape-polygon-svgrepo-com.svg" alt="">
-                            <h3>Fonctionnalité 1</h3>
-                        </section>
-                        <p>Description de la fonctionnalité et de la valeur ajoutée principale qu'elle apporte au client</p>
-                    </article>
-
-                
-                    <article>
-                        <section>
-                            <img src="assets/images/shape-polygon-svgrepo-com.svg" alt="">
-                            <h3>Fonctionnalité 2</h3>
-                        </section>
-                        <p>Description de la fonctionnalité et de la valeur ajoutée principale qu'elle apporte au client</p>
-                    </article>
-
-                
-                    <article>
-                        <section>
-                            <img src="assets/images/shape-polygon-svgrepo-com.svg" alt="">
-                            <h3>Fonctionnalité 3</h3>
-                        </section>
-                        <p>Description de la fonctionnalité et de la valeur ajoutée principale qu'elle apporte au client</p>
-                    </article>
-
-                
+    <!-- Hero banner -->
+    <section class="first-page">
+        <section class="hero-banner">
+            <section class="txt-1">
+                <article class="slide-txt">
+                    <div class="">Équipez</div>
+                    <div class="">Sautez</div>
+                    <div class="">Frappez</div>
+                    <div class="">Gagnez</div>
+                </article> 
                     
-                </section>
-
+                <h1>Prêt </br> à jouer ?</h1>
+                <button class="btn1">Découvrir les produits <i class="fa-solid fa-arrow-right-long"></i></button>
             </section>
-            <!-- Avantages -->
-            <section class="main-avantages">
-                <article>
-                    <section>
-                        <h3>Un titre pour transformer vos visiteurs en utilisateurs et obtenir des résultats</h3>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </section>
-                    <img src="assets/images/woman-playing-volleyball-beach.jpg" alt="">
-                </article>
-                <article>
-                    <img src="assets/images/woman-playing-volleyball-beach.jpg" alt="">
-                    <section>
-                        <h3>Un titre pour transformer vos visiteurs en utilisateurs et obtenir des résultats</h3>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </section>
-                </article>
+            <section class="txt-2">
+                <p><!--
+            Commencez dès maintenant à parcourir notre catalogue et trouvez l’équipement parfait pour votre prochain smash ! -->
+                </p>
+            </section>                
+        </section>
+        
+    </section>
+    <section>
+        <div class="cards-wrapper">
+            <p class="headline">alola. vous offre un choix rapide, fiable et 
+            
+                <span>
+                    de qualité supérieure pour équiper joueurs et équipes avec les meilleurs produits.
+            </span></p>
+            <article class="container-cards">
+                <div></div>
+                <div class="cards">
+                    <div class="card">
+                        <div class="icon"><i class="fa-solid fa-volleyball"></i></div>
+                        <div class="title">Large Choix & Flexibilité</div>
+                        <p class="desc">Un catalogue sur-mesure pour tous les passionnés de volley : ballons, chaussures, vêtements et plus.
+                        </p>
+                    </div>
+                    <div class="card">
+                        <div class="icon"><i class="fa-solid fa-check"></i></div>
+                        <div class="title">Jouez sans Attendre
+                        </div>
+                        <p class="desc">Accédez à nos produits dès aujourd’hui et équipez-vous rapidement avec des articles prêts à améliorer votre jeu.
+                        </p>
+                    </div>
+                    <div class="card">
+                        <div class="icon"><i class="fa-solid fa-angles-right"></i></div>
+                        <div class="title">Livraison Rapide et Fiable
+                        </div>
+                        <p class="desc">Recevez vos équipements sous quelques jours ouvrables. Jouez sans attendre !
+                        </p>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </section>
+    <!-- Avantages -->
+    <section class="main-avantages">
+        <article>
+            <section>
+                <h3>Un titre pour transformer vos visiteurs en utilisateurs et obtenir des résultats</h3>
+                <p>Lorem ipsum dolor sit amet.</p>
             </section>
-            <!-- Avis -->
-            <!-- Des chiffres -->
-             <section class="main-chiffres">
-                <article>
-                    <h3>20+</h3>
-                    <p>années d'expériences</p>
-                </article>
-                <article>
-                    <h3>8000+</h3>
-                    <p>Nombres d'objets vendus</p>
-                </article>
-                <article>
-                    <h3>7</h3>
-                    <p>Collaborateurs</p>
-                </article>
-                <article>
-                    <h3>95%</h3>
-                    <p>Satisfactions clients</p>
-                </article>
-             </section>
-            <!-- Foire aux questions -->
-             <section class="main-faq">
-                <h2>Foire aux questions</h2>
+            <img src="assets/images/woman-playing-volleyball-beach.jpg" alt="">
+        </article>
+        <article>
+            <img src="assets/images/woman-playing-volleyball-beach.jpg" alt="">
+            <section>
+                <h3>Un titre pour transformer vos visiteurs en utilisateurs et obtenir des résultats</h3>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </section>
+        </article>
+    </section>
+    <!-- Avis -->
+    <!-- Des chiffres -->
+     <section class="main-chiffres">
+        <article>
+            <h3>20+</h3>
+            <p>années d'expériences</p>
+        </article>
+        <article>
+            <h3>8000+</h3>
+            <p>Nombres d'objets vendus</p>
+        </article>
+        <article>
+            <h3>7</h3>
+            <p>Collaborateurs</p>
+        </article>
+        <article>
+            <h3>95%</h3>
+            <p>Satisfactions clients</p>
+        </article>
+     </section>
+    <!-- Foire aux questions -->
+     <section class="main-faq">
+        <h2>Foire aux questions</h2>
 
-                <article class="question">
-                    <section>
-                        <h3>Qui sommes nous ?</h3>
-                        <img src="assets/images/plus.svg" alt="plus">
-                    </section>
-                    <p>Non ! Si la mission de Riot vous passionne et que vous voulez aider les joueurs et joueuses, n'hésitez pas à postuler.</p>
-                </article>
-                
-                <article class="question">
-                    <section>
-                        <h3>Qui sommes nous ?</h3>
-                        <img src="assets/images/plus.svg" alt="plus">
-                    </section>
-                    <!-- <p></p> -->
-                </article>
+        <article class="question">
+            <section>
+                <h3>Qui sommes nous ?</h3>
+                <img src="assets/images/plus.svg" alt="plus">
+            </section>
+            <p>Non ! Si la mission de Riot vous passionne et que vous voulez aider les joueurs et joueuses, n'hésitez pas à postuler.</p>
+        </article>
+        
+        <article class="question">
+            <section>
+                <h3>Qui sommes nous ?</h3>
+                <img src="assets/images/plus.svg" alt="plus">
+            </section>
+            <!-- <p></p> -->
+        </article>
 
-                <article class="question">
-                    <section>
-                        <h3>Qui sommes nous ?</h3>
-                        <img src="assets/images/plus.svg" alt="plus">
-                    </section>
-                    <!-- <p>Non ! Si la mission de Riot vous passionne et que vous voulez aider les joueurs et joueuses, n'hésitez pas à postuler.</p> -->
-                </article>
-             </section>
+        <article class="question">
+            <section>
+                <h3>Qui sommes nous ?</h3>
+                <img src="assets/images/plus.svg" alt="plus">
+            </section>
+            <!-- <p>Non ! Si la mission de Riot vous passionne et que vous voulez aider les joueurs et joueuses, n'hésitez pas à postuler.</p> -->
+        </article>
+     </section>
 
-        </main>
+</main>
+<footer>
+    <section>
+        <article>
+            <h3 id="footer-logo">alola</h3>
+            <section>
+                <P id="footer-help">Besoin d'aide ?</P>
+                <a href="" id="footer-support">Go to Support</a>
+            </section>
+
+            <p>HEYYY</p>
+
+        </article>
+        <article>
+            <a href="" class="links">Home</a>
+            <a href="" class="links">Mises à jour</a>
+            <a href="" class="links">A propos</a>
+            <a href="" class="links">Contactez nous</a>
+            <a href="" class="links">Recrutement</a>
+        </article>
+        <article>
+            <a href="" class="links">Compte</a>
+            <a href="" class="links">Etat de commande</a>
+            <a href="" class="links">Aide</a>
+            <a href="" class="links">Politique de confidentialité</a>
+            <a href="" class="links">Notice légale</a>
+        </article>
+    </section>
+    <section>
+        <p id="copyright">© 2024 alola technologies.  All rights reserved.</p>
+    </section>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>
